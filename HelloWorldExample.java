f9300ba58feee596525c6154c7b6923e3fba4f9f
L00163434 <91318384+Snehalkhairnar9@users.noreply.github.com>
2021-12-09 19:48:23 +0000

    Create HelloWorldExample.java

---
+++ HelloWorldExample.java
@@ -0,0 +1,18 @@
+/**
+  * Utility class for extracting digits from integers.
+  *
+  * @version 1.0
+  */
+   class HelloWorldExample{ 
+       /**
+       * Tests the application through the main method.
+       *
+       * @param args command-line arguments
+       */
+        public static void main(String args[]){  
+          System.out.println("Hello LYIT, again");  
+          System.out.println("Some silly change");
+        }  
+    }  
+    
+© 2021 GitHub, Inc.
